4e12e0032b7760547c9ae689d13d3e8dac84a0a5
Senpai R <m_nour0@hotmail.com>
2025-10-11 08:00:06 +0300

    first upload

---
+++ My-CV-en/Js/script.js
@@ -0,0 +1,9 @@
+
+/*-- ================================== Typing animation ================================== --*/
+
+var typed = new Typed('.typing', {
+    strings:['','Web Designer','Web Developer','Graphic Design','Dealing with different systems'],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
